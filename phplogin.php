fc04c7620b3774b5f00009317570982ba2561c2a
mustafa <mustafaesam617@yahoo.com>
2024-01-12 14:38:47 +0300

    create database connection

---
+++ phplogin.php
@@ -0,0 +1,18 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header('Content-Type:application/json');
+$serverName='localhost';
+$dbName='streamingapp';
+$userName='root';
+$passWord='';
+//
+
+
+$connection=new PDO('mysql:host=localhost;dbname=kruxton','root','');
+$connection=new PDO('mysql:server='.$serverName.';dbname='.$dbName.'',$userName,$passWord);
+$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+
+
+
+
